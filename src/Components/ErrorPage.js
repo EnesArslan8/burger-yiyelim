5a107d8df64c1964b75c400915f48c69f541b866
EnesArslan8 <enes8arslan@gmail.com>
2023-07-24 18:31:25 +0300

    Error component and css finished finally Burger Yiyelim clone finished

--- src/Components/ErrorPage.js
+++ src/Components/ErrorPage.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
 
 function ErrorPage() {
   return (
-    <div>ErrorPage</div>
-  )
+    <div className="error">
+      <div className="container">
+        <p className="warning">!</p>
+        <div className="alert">
+            <h1>UYARI !</h1>
+            <p>Gitmeye çalıştığınız adres geçerli bir adres değildir. </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default ErrorPage
+export default ErrorPage;

8f8f3755da6e74e20efdf00e343334d91b0b087d
EnesArslan8 <enes8arslan@gmail.com>
2023-07-24 16:02:54 +0300

    AboutUs component finished and aboutUs css finished

--- src/Components/AboutUs.js
+++ src/Components/AboutUs.js
@@ -1,9 +1,66 @@
-import React from 'react'
+import React from "react";
 
 function AboutUs() {
   return (
-    <div>AboutUs</div>
-  )
+    <div className="aboutUs">
+      <div className="container">
+        <div className="about-img">
+          <img src="https://www.burgeryiyelim.com/templates/website/tema-1/assets/images/by_hk.png"></img>
+          <span className="about-about">HAKKIMIZDA</span>
+        </div>
+        <h2 className="about-title">Burger Yiyelim</h2>
+        <p className="about-description">
+          Keban Et 1976 yılında Keban Kasap olarak kuruldu. Butik bir kasap
+          olmasına rağmen müşterilerine sunmuş olduğu ürünlerin katkısız ve
+          lezzetli olmasından dolayı kısa sürede 'Et Ustası' ünvanının aldı. 41
+          yıldır iş hacmi, ürün yelpazesi, müşteriler, çalışanlar kısacası bir
+          çok şey değişti. Ancak değişmeyen tek şey Keban Et'in sunduğu et ve et
+          ürünlerinin lezzeti idi.
+          <br />
+          <br />
+          Keban Kasap müşterilerinden aldığı talepler ile kalitesini hiç
+          bozmadan büyüyebilen nadir et üreticilerindendir. İlerleyen yıllarda
+          gücünü müşterilerinin sadakat ve övgülerinden alarak üretim bandını
+          genişletti.
+          <br />
+          <br />
+          İstanbul'un en köklü kasapları arasında yer alan Keban Kasap şu anda
+          üretimine 1500m2 kapalı alana sahip Keban Et Entegre tesislerinde
+          devam etmekte.
+          <br />
+          <br />
+          Yakın zamana kadar sadece pişirmeye hazır et ürünleri üretmesine
+          rağmen 'Et Ustası' ünvanına yakışır bir şekilde tüketime hazır,
+          pişirilmiş et ürünleri üretmeye başladı. Halkımızın gerçek et,
+          katkısız lezzeti tatması için Et Yiyelim markasını oluşturdu. Bu marka
+          çok kısa sürede halkın beğenisini kazandı. Et Yiyelim lokantalarında,
+          kendine has lezzetleri içerisinde barındıran ürünleri ile müşterilerin
+          vazgeçilmezi arasında yer aldı.
+          <br />
+          <br />
+          Keban Et her zaman samimi aile kasabı çizgisinin yanında, modern
+          gelişimlere de önem veren bir firma olmuştur. Bu kimliğiyle 2016
+          yılında yerli burger projesi ile Burger Yiyelim markasını oluşturdu.
+          Burger Yiyelim'in farkı %100 katkısız %100 dana etinden yapılan
+          burgerleri oldu.
+        </p>
+        <span className="strongText"><strong >NEDEN UYGUN FİYAT?</strong></span>
+        <br />
+        <br />
+        <p>
+          Burger Yiyelim'in nefis köfteleri Keban Et 'in hijyenik ve son
+          teknoloji üretim tesislerinde üretilip oradan damaklarınıza kadar
+          ulaşmakta. Köftelerimizi ana markamız olan Keban Entegre Et
+          Tesisleri'nde hazırladığımız için sizlere daha uygun fiyata daha
+          kaliteli burgerler sunabiliyoruz.
+        </p>
+        <strong className="strongText">KÖFTELERİNİZ HELAL Mİ?</strong>
+        <br />
+        <br />
+        <p >Köftelerimiz TSE Helal belgelidir.</p>
+      </div>
+    </div>
+  );
 }
 
-export default AboutUs
+export default AboutUs;

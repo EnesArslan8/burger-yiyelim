a48ca9d31eabdeb8e4dc2371d7b0a400df49aa5d
EnesArslan8 <enes8arslan@gmail.com>
2023-07-24 16:37:17 +0300

    Products component and products css finished

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
 
 function Products() {
   return (
-    <div>Products</div>
-  )
+    <div className="products">
+      <div className="container">
+        <div className="boxOne">
+          <div className="box">
+            <img src="https://kebanet.restapp.com/uploads/36384/5a6c4f27abe8930a474ae667c93ad2fc-medium.png"></img>
+            <span>Patetes Bomba</span>
+          </div>
+          <div className="box">
+            <img src="https://kebanet.restapp.com/uploads/36384/362a6d5663c312a6e9b3fde150630f7a-medium.jpg"></img>
+            <span>Süper Avantajlı Menüler</span>
+          </div>
+          <div className="box">
+            <img src="https://kebanet.restapp.com/uploads/36384/2ec787a214d1de84204fb12044269b84-medium.jpg"></img>
+            <span>Avantajlı Menüler</span>
+          </div>
+          <div className="box">
+            <img src="https://kebanet.restapp.com/uploads/36384/7b4479264c906b977f261e4943f0a7cf-medium.jpg"></img>
+            <span>Kutu & Kova Tavuk Menüleri</span>
+          </div>
+        </div>
+        <div className="boxTwo">
+          <div className="box">
+            <img src="https://kebanet.restapp.com/uploads/36384/930199f3b958698a4fed5d68a7f3e55a-medium.jpg"></img>
+            <span>Burgerler</span>
+          </div>
+          <div className="box">
+            <img src="https://kebanet.restapp.com/uploads/36384/06eb7c955e75987846f03f8577ffdd70-medium.jpg"></img>
+            <span>Atıştırmalıklar</span>
+          </div>
+          <div className="box">
+            <img src="https://kebanet.restapp.com/uploads/36384/25ae7d40a43c0da451d7b1c8d44a971c-medium.jpeg"></img>
+            <span>Tatlı & Dondurma</span>
+          </div>
+          <div className="box">
+            <img src="https://kebanet.restapp.com/uploads/36384/9f6baa5a751a7466879c93a97db57572-medium.jpg"></img>
+            <span>İçecekler</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Products
+export default Products;
